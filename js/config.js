50ef3b5ae728f1acb9015469bbf1005a5c7de28e
Risou <71141301+Risou2333@users.noreply.github.com>
2025-12-22 22:28:16 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -4,9 +4,9 @@
  * 存放不会改变的常量和全局配置，例如 API 端点和下载源信息。
  */
 
-export const OSU_API_BASE_URL = 'https://osu.ppy.sh/api/v2';
-export const OSU_TOKEN_URL = 'https://osu.ppy.sh/oauth/token';
-export const CORS_PROXY_URL = 'https://cors.eu.org/';
+export const OSU_API_BASE_URL = '/osu-api/api/v2';
+export const OSU_TOKEN_URL = '/osu-token';
+export const CORS_PROXY_URL = ''; // 使用 Netlify 代理，设为空字符串
 
 export const DOWNLOAD_SOURCE_INFO = {
     nerinyan: { name: 'Nerinyan', url: 'https://api.nerinyan.moe/d/' },
